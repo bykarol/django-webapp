0913c617846185295bd314c64d74273db4a63a28
bykarol <brachokarol.sud@gmail.com>
2024-06-15 13:25:50 +0200

    starting again

--- patient/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-from django.db import models
-
-# Create your models here.
-class Patient(models.Model):
-    firstname = models.CharField(max_length=50)
-    lastname =  models.CharField(max_length=50)
-    address = models.TextField()
